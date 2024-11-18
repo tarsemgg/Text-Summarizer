87af1abf345554db22ee32ce463d7c7a96e79358
tarsemgg <tarsemj7@gmail.com>
2024-11-18 17:10:26 +0530

    requirements & installatios

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("Readme.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+
+__version__ = "0.0.0"
+
+
+REPO_NAME = "Text-Summarizer"
+AUTHOR_USER_NAME = "TARSEMGG"
+SRC_REPO = "text_summerizer"
+AUTHOR_EMAIL = "tarsemj7@gmail.com"
+
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A simple Python library for summarizing large text documents.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    package_dir=("":"src"),
+    packages=setuptools.find_packages(where="src")
+
+)
