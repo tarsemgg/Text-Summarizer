4eb7953ada9113a53bc53e5aea86f198908b0140
tarsemgg <tarsemj7@gmail.com>
2024-11-18 17:13:32 +0530

    requirements & installatios

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import setuptools
 
+# Read the long description from the README file
 with open("Readme.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-
-
+# Package version
 __version__ = "0.0.0"
 
-
+# Metadata
 REPO_NAME = "Text-Summarizer"
 AUTHOR_USER_NAME = "TARSEMGG"
 SRC_REPO = "text_summerizer"
 AUTHOR_EMAIL = "tarsemj7@gmail.com"
 
-
+# Setup function for setuptools
 setuptools.setup(
     name=REPO_NAME,
     version=__version__,
@@ -23,7 +23,6 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    package_dir=("":"src"),
-    packages=setuptools.find_packages(where="src")
-
+    package_dir={"": "src"},  # Fixed the typo here
+    packages=setuptools.find_packages(where="src"),  # Finds packages in the 'src' directory
 )
